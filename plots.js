57420939c6bc6e95b85b64502851058000a81f23
ramonmsa <ramonmoraissa@gmail.com>
2021-09-08 15:53:58 -0400

    updating files in order to match module

--- plots.js
+++ plots.js
@@ -1,53 +1,37 @@
+// Sort the data array using the greekSearchResults value
+data.sort(function(a, b) {
+  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
+});
 
-function init() {
-  var selector = d3.select("#selDataset");
+// Slice the first 10 objects for plotting
+data = data.slice(0, 10);
 
-  d3.json("samples.json").then((data) => {
-    console.log(data);
-    var sampleNames = data.names;
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
-})}
+// Reverse the array due to Plotly's defaults
+data = data.reverse();
 
-function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
-    var metadata = data.metadata;
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-	
-    var PANEL = d3.select("#sample-metadata");
-	PANEL.html("");
-	var table = PANEL.append("table")
-	console.log(resultArray);
-	for (const [key, value] of Object.entries(resultArray[0])) {
-	console.log(key, value);
-	var tr = table.append("tr")
-	tr.append("td").append('strong').text(key.toUpperCase() + " :")
-	tr.append("td").text(value)
-	
-	}
-    var result = resultArray[0];
+// Trace1 for the Greek Data
+var trace1 = {
+  x: data.map(row => row.greekSearchResults),
+  y: data.map(row => row.greekName),
+  text: data.map(row => row.greekName),
+  name: "Greek",
+  type: "bar",
+  orientation: "h"
+};
 
-    // PANEL.html("");
-    // PANEL.append("h6").text(result.location);
-	
-	// age: 34
-// bbtype: "I"
-// ethnicity: "Caucasian/Midleastern"
-// gender: "F"
-// id: 941
-// location: "Chicago/IL"
+// data
+var data = [trace1];
 
-	
-  });
-}
+// Apply the group bar mode to the layout
+var layout = {
+  title: "Greek gods search results",
+  margin: {
+    l: 100,
+    r: 100,
+    t: 100,
+    b: 100
+  }
+};
 
-function optionChanged(newSample) {
-  buildMetadata(newSample);
-  buildCharts(newSample);
-}
-
-init();
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", data, layout);
